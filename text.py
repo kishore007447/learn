c11c45333a6a04e63591a221a7d8b5c45eca7e81
kishore007447 <kishorebaskaran2006@gmail.com>
2025-11-12 19:10:42 +0530

    iteration

--- text.py
+++ text.py
@@ -1,5 +1,5 @@
 #lets check the github 
-
+'''
 l=[10,25,23,5]
 for i in l:
     if(i>18):
@@ -10,7 +10,7 @@ for i in l:
 #end of code
 
 #sha-1 program
-'''import hashlib
+import hashlib
 def hash_string(input_string):
     sha1_hash = hashlib.sha1()
     sha1_hash.update(input_string.encode())
@@ -18,6 +18,11 @@ def hash_string(input_string):
 input_str = "Hello, World!"
 hashed_str = hash_string(input_str)
 print(f"SHA-1 hash of '{input_str}': {hashed_str}")
+
 '''
 
 print('bye bye /......')
+
+
+for i in range(0,12):
+    print(i)
