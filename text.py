026fc909cabc961092153c88f48e576912584c22
kishore007447 <kishorebaskaran2006@gmail.com>
2025-11-12 15:41:37 +0530

    added SHA-1

--- text.py
+++ text.py
@@ -8,3 +8,13 @@ for i in l:
         print("not eligible to vote")
 
 #end of code
+
+#sha-1 program
+import hashlib
+def hash_string(input_string):
+    sha1_hash = hashlib.sha1()
+    sha1_hash.update(input_string.encode())
+    return sha1_hash.hexdigest()
+input_str = "Hello, World!"
+hashed_str = hash_string(input_str)
+print(f"SHA-1 hash of '{input_str}': {hashed_str}")
