196e33ac5a9b908e6aead441fb6a9efd72e015d1
kishore007447 <kishorebaskaran2006@gmail.com>
2025-11-23 10:28:35 +0530

    java events and actions

---
+++ ButtonExample.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class ButtonExample {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Button Action");
+        JButton button = new JButton("Click Me");
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Button clicked!");
+            }
+        });
+
+        frame.add(button);
+        frame.setSize(300, 200);
+        frame.setLayout(null);
+        button.setBounds(100, 70, 100, 30);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
